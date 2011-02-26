7fab4b0bd82558debb72fe78db6fe3e5be34d0ac
davidsulc <david@davidsulc.com>
2011-02-26 13:22:21 -0500

    initial commit

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,7 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+SugarOnRailsBasic::Application.config.secret_token = '0cc0c1e0ce2568cde7f1d68a7bb39997ce43985b74258ecec47e427682ab37ebd7612f70331ca203ba0fa8f0d6fbad4979a38f3a3e3e8f029ff17086048e6d08'
