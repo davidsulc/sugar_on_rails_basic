9b6185fc3929eb6e48427b3f1309ecc197152144
Carl Hicks <carl.hicks@gmail.com>
2011-02-27 00:44:25 -0800

    Added support for associations Removed prototype Refactored views

--- app/controllers/sugar_beans_controller.rb
+++ app/controllers/sugar_beans_controller.rb
@@ -2,18 +2,25 @@ class SugarBeansController < ApplicationController
   # This controller provides a generic interface to SugarCRM.  It expects
   # a module name to be passed as an argument
   def list
-    @module = params[:module]
+    @sugar_module = params[:module]
     begin
       @beans = class_for(params[:module]).all(:limit => 10)
     rescue NameError
-      @modules = available_modules.sort
+      @sugar_modules = available_modules.sort
       render :action => "available_modules"
     end
   end
   
   def show
-    @module = params[:module]
-    @bean   = class_for(params[:module]).find(params[:id])
+    @sugar_module = params[:module]
+    @bean         = class_for(params[:module]).find(params[:id])
+  end
+  
+  def show_associations
+    @sugar_module = params[:module]
+    @bean         = class_for(params[:module]).find(params[:id])
+    @association  = params[:association]
+    @beans        = @bean.send(@association.to_sym)
   end
   
   private 
--- config/routes.rb
+++ config/routes.rb
@@ -1,5 +1,6 @@
 SugarOnRailsBasic::Application.routes.draw do
   match ':module' => 'sugar_beans#list', :as => "module"
   match ':module/:id' => 'sugar_beans#show', :as => "sugar_bean"
+  match ':module/:id/:association' => 'sugar_beans#show_associations', :as => "sugar_bean_associations"
   root :to => "sugar_beans#list", :module => "accounts"
 end
