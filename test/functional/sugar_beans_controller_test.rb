1c7674bf876ad4255298f5072c92c98a0df95ff3
davidsulc <david@davidsulc.com>
2011-02-27 00:36:02 -0500

    add assertion

--- test/functional/sugar_beans_controller_test.rb
+++ test/functional/sugar_beans_controller_test.rb
@@ -8,6 +8,7 @@ class SugarBeansControllerTest < ActionController::TestCase
   
   test "should get users/1" do
     get(:show, {:module => "users", :id => "1"})
+    assert_equal SugarCRM::User.find(1), assigns(@bean)["bean"]
     assert_response :success
   end
 end
